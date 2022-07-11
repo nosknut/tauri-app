3f8ef90724c6ad3d893b3d56dc118a47fec1bcc6
nosknut <nosknut@gmail.com>
2022-07-11 18:54:44 +0200

    Fixes styling issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,49 @@
-import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, Container, Grid, Toolbar, Typography, useTheme } from '@mui/material';
+import { invoke } from '@tauri-apps/api';
 import { writeFile } from '@tauri-apps/api/fs';
 
 function App() {
+  const theme = useTheme()
   return (
-    <div>
-      <AppBar title="Hello World">
+    <div style={{ width: '100%', height: '100%' }}>
+      <AppBar title="Hello World" position='absolute'>
         <Toolbar>
-          <Typography variant="h2">Tauri App</Typography>
+          <Typography variant="h4">Tauri App</Typography>
         </Toolbar>
       </AppBar>
       <main style={{ width: '100%', height: '100%' }}>
-        <Box mx="auto" my="auto">
-          <Button
-            variant='contained'
-            color='primary'
-            onClick={async () => {
-              await writeFile('test.txt', 'Hello World')
-            }}
-          >
-            Write File
-          </Button>
-        </Box>
+        <Container maxWidth="sm" style={{ marginTop: theme.spacing(10) }}>
+          <Grid container direction="column" justifyContent="center" alignItems="center">
+            <Grid container spacing={3} direction="row" justifyContent="center" alignItems="center">
+              <Grid item sm={6} xs={12}>
+                <Button
+                  fullWidth
+                  variant='contained'
+                  color='primary'
+                  onClick={async () => {
+                    await writeFile('test.txt', 'Hello World')
+                  }}
+                >
+                  Write File
+                </Button>
+              </Grid>
+              <Grid item sm={6} xs={12}>
+                <Button
+                  fullWidth
+                  variant='contained'
+                  color='primary'
+                  onClick={async () => {
+                    await invoke('custom_read_file_function', { file: 'test.txt' })
+                  }}
+                >
+                  Write File with invoke
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
       </main>
-    </div>
+    </div >
   );
 }
 
