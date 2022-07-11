75fb05073d5d28175ec3bd124e0c0db4393140e8
nosknut <nosknut@gmail.com>
2022-07-11 19:48:22 +0200

    Fixes the error handling

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,18 +10,22 @@ use std::{
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![custom_read_file_function, replace])
+        .invoke_handler(tauri::generate_handler![
+            custom_read_file_function,
+            custom_write_file_function,
+            replace
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 
-    mainAction();
+    // main_action().expect("error while running main action");
 }
 
-fn mainAction() -> std::io::Result<()> {
-    let format_file = File::open("./TabExport.KSF")?;
-    let form = read_file(format_file);
-    let name_file = File::open("./names.txt")?;
-    let names = read_file(name_file);
+fn main_action() -> std::io::Result<()> {
+    let format_file = "./TabExport.KSF";
+    let form = custom_read_file_function(format_file).expect("Could not read format file");
+    let name_file = "./names.txt";
+    let names = custom_read_file_function(name_file).expect("Could not read name file");
     let lines = names.split('\n');
 
     let mut tabn: usize = 0;
@@ -29,8 +33,8 @@ fn mainAction() -> std::io::Result<()> {
         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
     ];
 
-    remove_dir_all("./KSF");
-    create_dir_all("./KSF");
+    remove_dir_all("./KSF").expect("Unable to remove KSF folder");
+    create_dir_all("./KSF").expect("Unable to create KSF folder");
 
     for (i, line) in lines.enumerate() {
         if i % 20 == 0 && i != 0 {
@@ -65,13 +69,38 @@ fn mainAction() -> std::io::Result<()> {
     Ok(())
 }
 
-fn read_file(file: File) -> String {
+#[tauri::command]
+fn custom_read_file_function(file_path: &str) -> Result<String, String> {
+    let file = match File::create(file_path) {
+        Ok(file) => file,
+        Err(err) => {
+            return Err(err.to_string());
+        }
+    };
     let mut buf_reader = BufReader::new(file);
     let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents);
-    return contents;
+    match buf_reader.read_to_string(&mut contents) {
+        Ok(_) => Ok(contents),
+        Err(err) => Err(err.to_string()),
+    }
 }
 
+#[tauri::command]
+fn custom_write_file_function(file_path: &str, content: &str) -> Result<(), String> {
+    let mut file = match File::create(file_path) {
+        Ok(file) => file,
+        Err(err) => {
+            return Err(err.to_string());
+        }
+    };
+
+    match file.write_all(content.as_bytes()) {
+        Ok(_) => Ok(()),
+        Err(err) => Err(err.to_string()),
+    }
+}
+
+#[tauri::command]
 fn replace(base: String, tabs: [&str; 20], i: usize) -> String {
     if i >= 20 {
         return base;
