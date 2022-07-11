da44ab680f98e048f86910d63c7d4e57404f33b9
nosknut <nosknut@gmail.com>
2022-07-11 21:20:49 +0200

    Adds open data dir button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { AppBar, Box, Button, Card, CardContent, CardHeader, Container, Grid, Toolbar, Typography, useTheme } from '@mui/material';
 import { invoke } from '@tauri-apps/api';
+import { open } from '@tauri-apps/api/dialog';
 import { readTextFile, writeFile } from '@tauri-apps/api/fs';
 import { dataDir, join } from '@tauri-apps/api/path';
 import { APPLICATION_NAME } from './constants';
@@ -8,6 +9,39 @@ function logAndRethrow(error: Error): never {
   console.error(error)
   alert(error)
   throw error
+}
+
+async function getDataDirPath() {
+  // Available standard app directories
+  // https://tauri.app/v1/api/js/enums/fs.basedirectory/
+  // How to get their path on different platforms
+  // https://tauri.app/v1/api/js/modules/path/
+  // How to enable access to them
+  // https://tauri.app/v1/api/js/modules/fs/
+  const dataDirPath = await dataDir()
+  return await join(dataDirPath, APPLICATION_NAME)
+}
+
+async function getTestFilePath() {
+  const dataDirPath = await getDataDirPath()
+  return await join(dataDirPath, 'test.txt')
+}
+
+function OpenDataDirButton() {
+  return (
+    <Button
+      fullWidth
+      variant="contained"
+      color="secondary"
+      onClick={async () => {
+        const dataDirPath = await getDataDirPath()
+        await open({
+          title: 'Data directory',
+          defaultPath: dataDirPath,
+        })
+      }}
+    >Open data dir</Button>
+  )
 }
 
 function App() {
@@ -32,15 +66,7 @@ function App() {
                       variant='contained'
                       color='primary'
                       onClick={async () => {
-                        // Available standard app directories
-                        // https://tauri.app/v1/api/js/enums/fs.basedirectory/
-                        // How to get their path on different platforms
-                        // https://tauri.app/v1/api/js/modules/path/
-                        // How to enable access to them
-                        // https://tauri.app/v1/api/js/modules/fs/
-
-                        const dataDirPath = await dataDir().catch(logAndRethrow)
-                        const filePath = await join(dataDirPath, APPLICATION_NAME, 'test.txt').catch(logAndRethrow)
+                        const filePath = await getTestFilePath().catch(logAndRethrow)
                         writeFile(filePath, 'Hello World').catch(logAndRethrow)
                       }}
                     >
@@ -52,19 +78,14 @@ function App() {
                       variant='contained'
                       color='primary'
                       onClick={async () => {
-                        // Available standard app directories
-                        // https://tauri.app/v1/api/js/enums/fs.basedirectory/
-                        // How to get their path on different platforms
-                        // https://tauri.app/v1/api/js/modules/path/
-                        // How to enable access to them
-                        // https://tauri.app/v1/api/js/modules/fs/
-                        const dataDirPath = await dataDir().catch(logAndRethrow)
-                        const filePath = await join(dataDirPath, APPLICATION_NAME, 'test.txt').catch(logAndRethrow)
+                        const filePath = await getTestFilePath().catch(logAndRethrow)
                         readTextFile(filePath).then(alert).catch(logAndRethrow)
                       }}
                     >
                       Read File
                     </Button>
+                    <Box mt={2} />
+                    <OpenDataDirButton />
                   </CardContent>
                 </Card>
               </Grid>
@@ -77,15 +98,7 @@ function App() {
                       variant='contained'
                       color='primary'
                       onClick={async () => {
-                        // Available standard app directories
-                        // https://tauri.app/v1/api/js/enums/fs.basedirectory/
-                        // How to get their path on different platforms
-                        // https://tauri.app/v1/api/js/modules/path/
-                        // How to enable access to them
-                        // https://tauri.app/v1/api/js/modules/fs/
-
-                        const dataDirPath = await dataDir().catch(logAndRethrow)
-                        const filePath = await join(dataDirPath, APPLICATION_NAME, 'test.txt').catch(logAndRethrow)
+                        const filePath = await getTestFilePath().catch(logAndRethrow)
                         invoke('custom_write_file_function', { filePath, content: 'Hello World Invoked' }).catch(logAndRethrow)
                       }}
                     >
@@ -97,20 +110,14 @@ function App() {
                       variant='contained'
                       color='primary'
                       onClick={async () => {
-                        // Available standard app directories
-                        // https://tauri.app/v1/api/js/enums/fs.basedirectory/
-                        // How to get their path on different platforms
-                        // https://tauri.app/v1/api/js/modules/path/
-                        // How to enable access to them
-                        // https://tauri.app/v1/api/js/modules/fs/
-
-                        const dataDirPath = await dataDir().catch(logAndRethrow)
-                        const filePath = await join(dataDirPath, APPLICATION_NAME, 'test.txt').catch(logAndRethrow)
+                        const filePath = await getTestFilePath().catch(logAndRethrow)
                         invoke('custom_read_file_function', { filePath }).then(alert).catch(logAndRethrow)
                       }}
                     >
                       Read File
                     </Button>
+                    <Box mt={2} />
+                    <OpenDataDirButton />
                   </CardContent>
                 </Card>
               </Grid>
